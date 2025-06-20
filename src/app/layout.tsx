52ac38fa3184df85fa84c6a8028f8f3c073e7ff2
Andy McPherson <mcphersonster@gmail.com>
2025-06-20 19:13:17 +0000

    Lets add a favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export const metadata: Metadata = {
     'AI tutor',
     'VerbaVivaEspañol',
   ],
+  icons: {
+    icon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🇪🇸</text></svg>',
+  },
 };
 
 export default function RootLayout({
